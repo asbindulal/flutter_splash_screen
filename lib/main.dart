82a23e5db33e5cb708c98acc1c4a7a4e4cedb801
asbindulal <dulalashi@gmail.com>
2022-09-01 18:42:45 +0545

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:splash_screen/homepage.dart';
+import 'package:splash_screen/splashscreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Splash Screen',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SplashScreen(),
+        '/homepage': (context) => const HomePage(),
+      },
+    );
+  }
+}
